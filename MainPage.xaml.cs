0055afc0e18d4892163fd33762a378936e219b0a
Vojtěch Charouz <vojtacharouz@gmail.com>
2023-11-07 11:27:06 +0100

    ui changes, backend prerp

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace todododolist;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace todododolist;
 
 public partial class MainPage : ContentPage
 {
@@ -7,8 +9,25 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+		List<ukol> ukoly = new List<ukol>();
+		ukol test = new ukol("test", new DateTime(2023, 11, 8), "hotovo");
+		ukoly.Add(test);
+		seznam.ItemsSource = ukoly;
 	}
 
 	
 }
 
+public class ukol
+{
+	public string nazev;
+	public DateTime termin;
+	public string status;
+
+	public ukol(string n, DateTime d, string s)
+	{
+		nazev = n;
+		termin = d;
+		status = s;
+	}
+}
